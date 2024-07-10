427f999ecea15ca3e6c53effdd6904d28160c301
yingjie_wang <2778809626@qq.com>
2024-07-10 15:00:12 +0800

    从机写寄存器

--- src/main.cpp
+++ src/main.cpp
@@ -7,164 +7,30 @@
 #include <iostream>
 #include <modbus.h>
 
+#define SERVER_ID 1
+#define UT_REGISTERS_ADDRESS 0
+#define UT_REGISTERS_NB_MAX 10
 
-int main()
+
+
+#include <thread>
+
+// 假设ModbusSlave类和其他必要的头文件已经被包含
+
+int main() 
 {
-    try
-    {
-        const int slave_id = 1;   // Modbus从机地址
-        const int start_addr = 0; // 起始寄存器地址
-        const int num_regs = 10;  // 要读取或写入的寄存器数量
-        ModbusTool tool("COM6", 9600, 'N', 8, 1);
+    // 创建ModbusSlave实例，服务器ID为1，最多有100个寄存器
+    ModbusSlave slave(1, 100);
 
-        // 写线圈
-        tool.pollWriteCoils(slave_id, 0, 5);
+    // 初始化Modbus上下文，连接到串口"COM6"，波特率9600，数据位8，无奇偶校验，停止位1
+    slave.initialize("COM6", 9600, 'N', 8, 1);
 
-        // 读取线圈示例
-        uint8_t read_tab_coils[5]; // 读取结果存储数组
-        //tool.pollReadCoils(slave_id, 0, 5); // 读取从地址 0 开始的 5 个线圈
-
-        //// 主模式 写入数据到从机寄存器
-        //tool.pollWriteRegisters(slave_id, start_addr, num_regs);
-
-        //// 主模式 读取从机寄存器数据
-        //tool.pollReadRegisters(slave_id, start_addr, num_regs);
-        ////Registers read :
-        ////Register 0 : 1
-        ////Register 1 : 2
-        ////Register 2 : 3
-        ////Register 3 : 4
-        ////Register 4 : 5
-        ////Register 5 : 6
-        ////Register 6 : 7
-        ////Register 7 : 8
-        ////Register 8 : 9
-        ////Register 9 : 10
-
-    }
-    catch (const std::exception& e)
-    {
-        std::cout << "Exception caught: " << e.what() << std::endl;
-        return 1;
-    }
+    // 启动一个线程来处理Modbus请求
+    slave.handleRequests();
 
     return 0;
 }
 
-
-
-
-
-
-
-////// 主模式(服务端写入数据到从机寄存器)
-//int main() 
-//{
-//    modbus_t* ctx;
-//    int rc;
-//    const int slave_id = 1; // Modbus从机地址
-//    const int start_addr = 0; // 起始寄存器地址
-//    const int num_regs = 10; // 要读取的寄存器数量
-//    uint16_t tab_reg[32]; // 用于存储读取到的寄存器数据
-//
-//    // 创建Modbus RTU上下文
-//    ctx = modbus_new_rtu("COM6", 9600, 'N', 8, 1);
-//    if (ctx == NULL) {
-//        std::cout << "Unable to create the libmodbus context" << std::endl;
-//        return 1;
-//    }
-//
-//    // 打开串口设备
-//    if (modbus_connect(ctx) == -1) {
-//        std::cout << "Connection failed: " << modbus_strerror(errno) << std::endl;
-//        modbus_free(ctx);
-//        return 1;
-//    }
-//
-//    // 设置从机地址
-//    modbus_set_slave(ctx, slave_id);
-//
-//    // 循环读取寄存器数据
-//    while (true)
-//    {
-//        // 写入数据到寄存器
-//        for (int i = 0; i < num_regs; ++i) {
-//            tab_reg[i] = i + 1; // 模拟写入数据，这里写入了1到10
-//        }
-//
-//        rc = modbus_write_registers(ctx, start_addr, num_regs, tab_reg);
-//        if (rc == -1)
-//        {
-//            std::cout << "Write registers failed: " << modbus_strerror(errno) << std::endl;
-//            continue; // 跳出循环或者处理错误
-//        }
-//
-//        std::cout << "Registers written successfully." << std::endl;
-//
-//        // 可选：等待一段时间后再次写入
-//        std::this_thread::sleep_for(std::chrono::seconds(3));
-//    }
-//
-//    // 关闭Modbus连接
-//    modbus_close(ctx);
-//    modbus_free(ctx);
-//
-//    return 0;
-//}
-
-
-//// 主模式(服务端读取客户端线圈地址)
-//int main() {
-//    modbus_t* ctx;
-//    int rc;
-//    uint16_t tab_reg[32]; // 用于存储读取到的寄存器数据
-//    const int slave_id = 1; // Modbus从机地址
-//    const int start_addr = 0; // 起始寄存器地址
-//    const int num_regs = 10; // 要读取的寄存器数量
-//
-//    // 创建Modbus RTU上下文
-//    ctx = modbus_new_rtu("COM6", 9600, 'N', 8, 1);
-//    if (ctx == NULL) {
-//        std::cout << "Unable to create the libmodbus context" << std::endl;
-//        return 1;
-//    }
-//
-//    // 打开串口设备
-//    if (modbus_connect(ctx) == -1) {
-//        std::cout << "Connection failed: " << modbus_strerror(errno) << std::endl;
-//        modbus_free(ctx);
-//        return 1;
-//    }
-//
-//    // 设置从机地址
-//    modbus_set_slave(ctx, slave_id);
-//
-//    // 循环读取寄存器数据
-//    while (true) 
-//    {
-//        rc = modbus_read_registers(ctx, start_addr, num_regs, tab_reg);
-//        if (rc == -1) 
-//        {
-//            std::cout << "Read registers failed: " << modbus_strerror(errno) << std::endl;
-//            continue; // 跳出循环或者处理错误
-//        }
-//
-//        // 打印读取到的寄存器数据
-//        std::cout << "Registers read:" << std::endl;
-//        for (int i = 0; i < num_regs; ++i) {
-//            std::cout << "Register " << start_addr + i << ": " << tab_reg[i] << std::endl;
-//        }
-//
-//        // 可选：等待一段时间后再次读取
-//        std::this_thread::sleep_for(std::chrono::seconds(3));
-//    }
-//
-//    // 关闭Modbus连接
-//    modbus_close(ctx);
-//    modbus_free(ctx);
-//
-//    return 0;
-//}
 
 
 
@@ -176,28 +42,224 @@ int main()
 
 //int main()
 //{
+//    modbus_t* ctx = NULL;
+//    uint8_t* query = NULL;
+//    modbus_mapping_t* mapping = NULL;
+//    int ret;
 //
-//	Config config;
-//#ifndef NDEBUG
-//	std::string configPath = "../../../../Config/my_config.json";
-//#else
-//	std::string configPath = "./my_config.json";
-//#endif
-//    if (config.read_config(configPath))
+//    // 初始化Modbus上下文为RTU模式，连接到串口"COM6"，波特率9600，数据位8，无奇偶校验，停止位1
+//    ctx = modbus_new_rtu("COM6", 9600, 'N', 8, 1);
+//    if (ctx == NULL) 
 //    {
-//        std::cout << "Read config file succession " << std::endl;
+//        fprintf(stderr, "Failed to create Modbus context: %s\n", modbus_strerror(errno));
+//        return EXIT_FAILURE;
 //    }
-//    else
+//
+//    // 设置从机地址
+//    modbus_set_slave(ctx, SERVER_ID);
+//
+//    // 分配查询缓冲区
+//    query = (uint8_t*)malloc(MODBUS_RTU_MAX_ADU_LENGTH);
+//    if (query == NULL) 
 //    {
-//        std::cout << "ERROR : Failed to read config file " << std::endl;
+//        fprintf(stderr, "Failed to allocate memory for query buffer\n");
+//        modbus_free(ctx);
+//        return EXIT_FAILURE;
+//    }
+//
+//    // 创建Modbus映射，定义了保持寄存器的数量为UT_REGISTERS_NB_MAX
+//    mapping = modbus_mapping_new(0, 0, UT_REGISTERS_NB_MAX, 0);
+//    if (mapping == NULL) 
+//    {
+//        fprintf(stderr, "Failed to allocate Modbus mapping: %s\n", modbus_strerror(errno));
+//        free(query);
+//        modbus_free(ctx);
+//        return EXIT_FAILURE;
+//    }
+//
+//    // 连接到串行设备
+//    if (modbus_connect(ctx) == -1) 
+//    {
+//        fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+//        modbus_mapping_free(mapping);
+//        free(query);
+//        modbus_free(ctx);
+//        return EXIT_FAILURE;
+//    }
+//
+//    // 主循环处理请求
+//    while (1) 
+//    {
+//        // 接收Modbus请求
+//        ret = modbus_receive(ctx, query);
+//        if (ret == -1) 
+//        {
+//            fprintf(stderr, "Receive error: %s\n", modbus_strerror(errno));
+//            break;
+//        }
+//        else 
+//        {
+//            //printf("Received bytes: %d\n", ret);
+//            //for (int i = 0; i < ret; i++)
+//            //{
+//            //    printf("%x ", query[i]);
+//            //}
+//            //std::cout << std::endl;
+//
+//            uint8_t slaveId;
+//            uint8_t function_code;
+//            uint16_t startAddress; // 获取寄存器地址
+//            uint16_t endAddress; // 获取写入的值
+//            uint16_t crc; // 获取写入的值
+//            if (ret >= 8)
+//            {
+//                slaveId = query[0];
+//                function_code = query[1]; // 获取Modbus功能码
+//                startAddress = (query[2] << 8) | query[3]; // 获取寄存器地址
+//                endAddress = (query[4] << 8) | query[5]; // 获取写入的值
+//                crc = (query[6] << 8) | query[7]; // 获取写入的值
+//            }
+//
+//            switch (function_code) 
+//            {
+//            case MODBUS_FC_READ_HOLDING_REGISTERS: // 0x03 // // 主机是03读取寄存器,这里就应该是写寄存器 
+//                
+//                // 写入保持寄存器
+//                // 检查地址是否在有效范围内
+//                if (startAddress >= 0 && startAddress < UT_REGISTERS_NB_MAX)
+//                {
+//                    // 将0~9 写入1~10
+//                    for (int i = 1; i <= UT_REGISTERS_NB_MAX; i++)
+//                    {
+//                        mapping->tab_registers[i-1] = i;
+//                    }
+//                    // 发送响应
+//                    modbus_reply(ctx, query, ret, mapping);
+//                }
+//                else 
+//                {
+//                    // 发送非法数据地址错误响应
+//                    modbus_reply_exception(ctx, query, MODBUS_EXCEPTION_ILLEGAL_DATA_ADDRESS);
+//                }
+//                break;
+//
+//            case MODBUS_FC_WRITE_SINGLE_REGISTER: // 0x06
+//                
+//                // 获取读取寄存器的起始地址
+//                if (startAddress >= 0 && startAddress < UT_REGISTERS_NB_MAX)
+//                {
+//                    // 读取保持寄存器的值
+//                    uint16_t num_registers = (query[4] << 8) | query[5]; // 要读取的寄存器数量
+//                    uint8_t response_length = 1 + num_registers * 2; // 响应长度
+//
+//                    // 确保请求的寄存器数量在有效范围内
+//                    if (num_registers > 0 && num_registers <= UT_REGISTERS_NB_MAX - startAddress)
+//                    {
+//                        // 构造响应报文
+//                        query[2] = response_length - 1; // 更新响应长度字段
+//                        query[3] = num_registers * 2; // 更新数据长度字段
+//
+//                        // 依次将寄存器值写入响应报文
+//                        for (int i = 0; i < num_registers; ++i) {
+//                            uint16_t reg_value = mapping->tab_registers[startAddress + i];
+//                            query[3 + i * 2 + 1] = reg_value >> 8; // 高字节
+//                            query[3 + i * 2 + 2] = reg_value & 0xFF; // 低字节
+//                        }
+//
+//                        // 发送响应
+//                        modbus_reply(ctx, query, ret, mapping);
+//                    }
+//                    else {
+//                        // 发送非法数据地址错误响应
+//                        modbus_reply_exception(ctx, query, MODBUS_EXCEPTION_ILLEGAL_DATA_ADDRESS);
+//                    }
+//                }
+//                else {
+//                    // 发送非法数据地址错误响应
+//                    modbus_reply_exception(ctx, query, MODBUS_EXCEPTION_ILLEGAL_DATA_ADDRESS);
+//                }
+//                break;
+//            default:
+//                // 发送不支持功能码错误响应
+//                modbus_reply_exception(ctx, query, MODBUS_EXCEPTION_ILLEGAL_FUNCTION);
+//                break;
+//            }
+//        }
+//    }
+//
+//
+//    // 清理
+//    modbus_mapping_free(mapping);
+//    free(query);
+//    modbus_close(ctx);
+//    modbus_free(ctx);
+//
+//    return EXIT_SUCCESS;
+//}
+
+
+
+
+
+
+
+//int main()
+//{
+//    try
+//    {
+//        const int slave_id = 1;   // Modbus从机地址
+//        const int start_addr = 0; // 起始寄存器地址
+//        const int num_regs = 10;  // 要读取或写入的寄存器数量
+//        ModbusPoll tool("COM6", 9600, 'N', 8, 1);
+//
+//        // 写线圈
+//        tool.pollWriteCoils(slave_id, 0, 5);
+//
+//        // 读取线圈示例
+//        uint8_t read_tab_coils[5]; // 读取结果存储数组
+//        //tool.pollReadCoils(slave_id, 0, 5); // 读取从地址 0 开始的 5 个线圈
+//
+//        //// 主模式 写入数据到从机寄存器
+//        //tool.pollWriteRegisters(slave_id, start_addr, num_regs);
+//
+//        //// 主模式 读取从机寄存器数据
+//        //tool.pollReadRegisters(slave_id, start_addr, num_regs);
+//        ////Registers read :
+//        ////Register 0 : 1
+//        ////Register 1 : 2
+//        ////Register 2 : 3
+//        ////Register 3 : 4
+//        ////Register 4 : 5
+//        ////Register 5 : 6
+//        ////Register 6 : 7
+//        ////Register 7 : 8
+//        ////Register 8 : 9
+//        ////Register 9 : 10
+//
+//    }
+//    catch (const std::exception& e)
+//    {
+//        std::cout << "Exception caught: " << e.what() << std::endl;
 //        return 1;
 //    }
 //
-//    std::filesystem::path data_1(DEFAULT_DATA_DIR);
-//    data_1 += "/geo_db/example6.db";
-//
-//	modbus_tool tc;
-//
-//
-//	return 0;
+//    return 0;
 //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/modbus/modbusTool.h
+++ src/modbus/modbusTool.h
@@ -7,7 +7,10 @@
 #include <modbus.h>
 
 
-class ModbusTool
+/// <summary>
+/// modbus 主机模式
+/// </summary>
+class ModbusPoll
 {
 private:
 	modbus_t* ctx;
@@ -22,7 +25,7 @@ public:
 	/// <param name="parity">奇偶校验位（'N'表示无校验）</param>
 	/// <param name="data_bits">数据位（8位）</param>
 	/// <param name="stop_bits">停止位（1位）</param>
-	ModbusTool(const char* device, int baud, char parity, int data_bits, int stop_bits)
+	ModbusPoll(const char* device, int baud, char parity, int data_bits, int stop_bits)
 	{
 		// 创建Modbus RTU上下文
 		ctx = modbus_new_rtu(device, baud, parity, data_bits, stop_bits);
@@ -33,7 +36,7 @@ public:
 		}
 	}
 
-	~ModbusTool()
+	~ModbusPoll()
 	{
 		// 关闭Modbus连接并释放资源
 		modbus_close(ctx);
@@ -44,11 +47,10 @@ public:
 	/// 主模式 写入数据到从机寄存器
 	/// 查看使用 modbus slave: 
 	///     Slave Definition:(功能码)03:Holding Register(04)
-	///     
 	/// </summary>
-	/// <param name="slave_id"></param>
-	/// <param name="start_addr"></param>
-	/// <param name="num_regs"></param>
+	/// <param name="slave_id">Modbus从机地址</param>
+	/// <param name="start_addr">起始寄存器地址</param>
+	/// <param name="num_regs">要读取或写入的寄存器数量</param>
 	void pollWriteRegisters(int slave_id, int start_addr, int num_regs)
 	{
 		uint16_t tab_reg[32];     // 用于存储读取到的寄存器数据
@@ -87,6 +89,7 @@ public:
 
 	/// <summary>
 	/// 主模式 读取从机寄存器数据
+	/// 查看使用 modbus slave: 
 	/// </summary>
 	/// <param name="slave_id">Modbus从机地址</param>
 	/// <param name="start_addr">起始寄存器地址</param>
@@ -128,6 +131,7 @@ public:
 
 	/// <summary>
 	/// 主模式 写入线圈数据到从机线圈
+	/// 查看使用 modbus slave: 
 	/// </summary>
 	/// <param name="slave_id"></param>
 	/// <param name="coil_addr"></param>
@@ -170,7 +174,7 @@ public:
 
 	/// <summary>
 	/// 主模式 读取从机线圈数据
-	/// 
+	/// 查看使用 modbus slave: 
 	/// </summary>
 	/// <param name="slave_id">Modbus从机地址</param>
 	/// <param name="start_addr">起始线圈地址</param>
@@ -211,11 +215,173 @@ public:
 		//modbus_free(ctx);
 	}
 
+
+
 };
 
 
 
+class ModbusSlave 
+{
+private:
+	modbus_t* context;
+	uint8_t* queryBuffer;
+	modbus_mapping_t* mapping;
+	const uint16_t registersNbMax;
+	const uint8_t slaveId;
 
+	/// <summary>
+	/// 写入数据到寄存器
+	/// 对应主机功能码为度寄存器(03),主机是读,这里就是写
+	/// </summary>
+	/// <param name="startAddress"></param>
+	/// <param name="mapping"></param>
+	void writeHoldingRegisters(uint16_t startAddress, modbus_mapping_t* mapping)
+	{
+		if (startAddress >= 0 && startAddress < registersNbMax)
+		{
+			modbus_reply(context, queryBuffer, 8, mapping);
+		}
+		else 
+		{
+			modbus_reply_exception(context, queryBuffer, MODBUS_EXCEPTION_ILLEGAL_DATA_ADDRESS);
+		}
+	}
+
+	// 预留的函数框架
+	void writeCoil(uint16_t startAddress, bool value)
+	{
+
+	}
+
+	uint16_t readHoldingRegister(uint16_t registerAddress) 
+	{
+		// 实现读寄存器功能
+		return 0;
+	}
+
+	bool readDiscreteInput(uint16_t inputAddress) 
+	{
+		// 实现读线圈功能
+		return false;
+	}
+
+public:
+
+	ModbusSlave(uint8_t serverId, uint16_t registersNbMax)
+		: serverId(serverId), registersNbMax(registersNbMax), context(nullptr), queryBuffer(nullptr), mapping(nullptr) {}
+
+	~ModbusSlave() 
+	{
+		this->cleanup();
+	}
+
+	void initialize(const char* port, int baudrate, char parity, int databits, int stopbits)
+	{
+		if (!context)
+		{
+			context = modbus_new_rtu(port, baudrate, parity, databits, stopbits);
+			if (!context)
+			{
+				std::cerr << "Failed to create Modbus context: " << modbus_strerror(errno) << std::endl;
+				return;
+			}
+			modbus_set_slave(context, serverId);
+			queryBuffer = (uint8_t*)malloc(MODBUS_RTU_MAX_ADU_LENGTH);
+			if (!queryBuffer)
+			{
+				std::cerr << "Failed to allocate memory for query buffer" << std::endl;
+				return;
+			}
+			mapping = modbus_mapping_new(0, 0, registersNbMax, 0);
+			if (!mapping)
+			{
+				std::cerr << "Failed to allocate Modbus mapping: " << modbus_strerror(errno) << std::endl;
+				return;
+			}
+			if (modbus_connect(context) == -1)
+			{
+				std::cerr << "Connection failed: " << modbus_strerror(errno) << std::endl;
+				return;
+			}
+		}
+	}
+
+	void handleRequests()
+	{
+		while (true)
+		{
+			int ret = modbus_receive(context, queryBuffer);
+			if (ret == -1)
+			{
+				std::cerr << "Receive error: " << modbus_strerror(errno) << std::endl;
+				break;
+			}
+			else
+			{
+				handleRequest(queryBuffer, ret);
+			}
+		}
+	}
+
+	void handleRequest(uint8_t* request, int length) 
+	{
+		if (length >= 8) 
+		{
+			
+			//uint8_t functionCode = request[1];
+			//uint16_t address = (request[2] << 8) | request[3];
+
+			slaveId = request[0];
+			uint8_t function_code = request[1]; // 获取Modbus功能码
+			uint16_t startAddress = (request[2] << 8) | request[3]; // 获取寄存器地址
+			uint16_t endAddress = (request[4] << 8) | request[5]; // 获取写入的值
+			uint16_t crc = (request[6] << 8) | request[7]; // 获取写入的值
+
+			for (int i = 1; i <= registersNbMax; i++)
+			{
+				mapping->tab_registers[i - 1] = i;
+			}
+
+			switch (function_code) {
+			case MODBUS_FC_READ_COILS:
+				// 调用writeCoil函数
+
+				break;
+			case MODBUS_FC_READ_HOLDING_REGISTERS:   // 主机是03读取寄存器,这里就应该是写寄存器 
+				writeHoldingRegisters(startAddress, mapping);
+				break;
+			case MODBUS_FC_WRITE_SINGLE_REGISTER:
+				// 调用readHoldingRegister函数
+				break;
+			case MODBUS_FC_READ_DISCRETE_INPUTS:
+				// 调用readDiscreteInput函数
+				break;
+			default:
+				modbus_reply_exception(context, request, MODBUS_EXCEPTION_ILLEGAL_FUNCTION);
+				break;
+			}
+		}
+	}
+
+	void cleanup() 
+	{
+		if (mapping)
+		{
+			modbus_mapping_free(mapping);
+		}
+		if (queryBuffer)
+		{
+			free(queryBuffer);
+		}
+		if (context)
+		{
+			modbus_close(context);
+			modbus_free(context);
+		}
+	}
+
+};
 
 
 
